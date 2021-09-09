96f5d1248ce912dbfc02f01041df08dc83e1f535
Keigal <oronoco.alex@gmail.com>
2021-09-09 14:18:41 -0500

    added line to fix engine

--- client/main.lua
+++ client/main.lua
@@ -10,10 +10,14 @@ AddEventHandler('repairtest:fixVehicle', function()
     -- Change false to true if you want to enable repair for last vehicle player was in
     local vehicle = GetVehiclePedIsIn(playerPed, false)
 
+    -- Sets the vehicle's engine to full health
+    SetVehicleEngineHealth(vehicle, 1000)
+
     -- Sets vehicle back to full health, however it will not fix a broken engine.
     SetVehicleFixed(vehicle)
 
     -- Sets the cosmetics of the vehicle to look repaired
     SetVehicleDeformationFixed(vehicle)
+    Engine
 
 end)
