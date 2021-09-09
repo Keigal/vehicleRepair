d631bc2725e2c5cfa4b26d719733a50343c17916
Keigal <oronoco.alex@gmail.com>
2021-09-09 14:08:34 -0500

    resource to fix vehicles

---
+++ client/main.lua
@@ -0,0 +1,19 @@
+
+-- Registering the command /fixVehicle
+RegisterNetEvent('repairtest:fixVehicle')
+AddEventHandler('repairtest:fixVehicle', function()
+
+    -- Selects playerPed that typed the command
+    local playerPed = GetPlayerPed(-1)
+
+    -- Gets the vehicle the player is currently in
+    -- Change false to true if you want to enable repair for last vehicle player was in
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    -- Sets vehicle back to full health, however it will not fix a broken engine.
+    SetVehicleFixed(vehicle)
+
+    -- Sets the cosmetics of the vehicle to look repaired
+    SetVehicleDeformationFixed(vehicle)
+
+end)
