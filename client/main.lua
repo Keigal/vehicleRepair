a7856153753ec8727e63af3cab6eec77f4cd6413
Keigal <34088122+Keigal@users.noreply.github.com>
2021-09-09 22:33:06 -0500

    added two different methods to hopefully fix cars on a more server-sided way

--- client/main.lua
+++ client/main.lua
@@ -3,6 +3,8 @@
 -- Creates thread that adds command to suggested commands
 Citizen.CreateThread(function()
     TriggerEvent('chat:addSuggestion', '/fv', 'Fully repairs a vehicle.')
+    TriggerEvent('chat:addSuggestion', '/sfv', 'Server-sided command to fully repair vehicle.')
+    TriggerEvent('chat:addSuggestion', 'globalfv', 'Runs fix vehicle event on ALL clients.')
 end)
 
 -- Creating event to fix a vehicle
@@ -37,8 +39,39 @@ AddEventHandler('vehicleRepair:fv', function()
     -- Sets the cosmetics of the vehicle to look fixed
     SetVehicleDeformationFixed(vehicle)
 
-    -- Tells player the vehicle has been fixed
-    notification("Vehicle fixed!")
+end)
 
+-- Created this copy so I can save time testing using two different methods.
+-- Creating event to fix a vehicle
+RegisterNetEvent('vehicleRepair:cfv')
+AddEventHandler('vehicleRepair:cfv', function(vehicle)
+
+    -- Selects playerPed that typed the command
+    -- local playerPed = GetPlayerPed(-1)
+
+    -- Gets the vehicle the player is currently in
+    -- Change true to false if you want to disable repair for last vehicle player was in
+    -- local vehicle = GetVehiclePedIsIn(playerPed, true)
+
+    -- Checks if vehicle is on fire, extinguishes if it is.
+    if IsEntityOnFire(vehicle)
+    then
+        -- Event only needs vehicle coords if there is a fire
+        local coords = GetEntityCoords(vehicle)
+        StopFireInRange(coords, 20)
+        StopEntityFire(vehicle)
+    end
+
+    -- Ensures vehicle is driveable
+    SetVehicleUndriveable(vehicle, false)
+
+    -- Sets the vehicle's engine to full health
+    SetVehicleEngineHealth(vehicle, 1000)
+
+    -- Sets vehicle back to full health, however it will not fix a broken engine.
+    SetVehicleFixed(vehicle)
+
+    -- Sets the cosmetics of the vehicle to look fixed
+    SetVehicleDeformationFixed(vehicle)
 
 end)
--- server/main.lua
+++ server/main.lua
@@ -4,3 +4,35 @@ RegisterCommand('fv', function(source)
     TriggerClientEvent('vehicleRepair:fv', source)
     
 end, false)
+
+-- Register command for fix that forces event to all clients
+RegisterCommand('globalfv', function(source)
+
+    TriggerClientEvent('vehicleRepair:fv', -1)
+
+end, false)
+
+-- Register sfv reworked command
+RegisterCommand('sfv', function(source)
+
+    TriggerEvent('vehiclerepair:sfv')
+
+end, false)
+
+-- Rework of the previous fv command, but server-side.
+RegisterNetEvent('vehiclerepair:sfv')
+AddEventHandler('vehiclerepair:sfv', function()
+
+    -- Get player who ran command
+    local playerPed = GetPlayerPed(-1)
+
+    -- Get vehicle player was last in
+    local vehicle = GetVehiclePedIsIn(playerPed, true)
+
+    -- Runs previous fv event to fix the vehicle client-side
+    TriggerClientEvent('vehicleRepair:cfv', source, vehicle)
+
+    -- Fixes vehicle body on the server side
+    SetVehicleBodyHealth(vehicle, 1000)
+
+end)
